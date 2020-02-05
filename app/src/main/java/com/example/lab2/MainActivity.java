837f68f51e82e6cf37c106a3a8109b1e2c508347
nbhoff@wisc.edu <>
2020-02-05 15:12:20 -0600

    Working Toast (Through Q14)

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
         Log.i("Info", "Button was clicked!");
+        EditText myEditText = findViewById(R.id.myEditText);
+        String toastText = myEditText.getText().toString();
+        Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_LONG).show();
     }
 
     @Override
