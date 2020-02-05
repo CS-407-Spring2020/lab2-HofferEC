045a1a76401a6ad1f0cf86b6ad0148de3dde4a50
nbhoff@wisc.edu <>
2020-02-05 15:00:04 -0600

    Working button (through Q8)

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        Log.i("Info", "Button was clicked!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
